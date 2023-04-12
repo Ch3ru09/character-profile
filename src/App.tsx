fec6b2f0f48f7156b433ff7cd4f2e39f2e8c6660
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-04-12 16:52:04 -0400

    added hobbies section

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import Header from "./components/Header";
+import Quotes from "./components/Quotes";
+import type { ReactNode } from "react";
 
 export default function App() {
   return (
     <div className="bg-hero-zig-zag">
       <main className="font-jost hyphens-manual">
-        <Page>
+        <Page marginSide={"r"}>
           <Header />
+          <Quotes />
         </Page>
-        <Page></Page>
+        <Page marginSide={"l"}></Page>
       </main>
     </div>
   );
 }
 
-function Page({ children }: PageProps) {
-  console.log(children);
-
-  return (
-    <div className="max-w-3xl p-3 mx-auto my-auto bg-gray-100 border-4 border-gray-700 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 w-letter rounded-2xl print:bg-white">
-      {children}
-    </div>
-  );
+function Page({ children, marginSide }: PageProps) {
+  return <div className={`p-3 m${marginSide}-0 mx-auto my-6 bg-gray-100 border-4 border-gray-700 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 w-letter rounded-2xl print:bg-white`}>{children}</div>;
 }
 
 interface PageProps {
-  children?: React.ReactNode
+  children?: ReactNode | ReactNode[];
+  marginSide?: string;
 }
+

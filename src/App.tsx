49203837628c55065c9ebaa4e3d59af83f6ed584
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-04-12 14:47:38 -0400

    remade page element

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import Header from "./Header";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="bg-hero-zig-zag">
       <main className="font-jost hyphens-manual">
-        <Page />
-        <Page />
+        <Page>
+          <Header />
+        </Page>
+        <Page></Page>
       </main>
     </div>
   );
 }
-function Page() {
+
+function Page({ children }: PageProps) {
+  console.log(children);
+
   return (
-    <div className="max-w-3xl p-3 mx-auto my-auto bg-gray-100 border-4 border-gray-700 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 print:my-o lg:h-letter md:max-w-letter md:h-letter xsm:p-8 sm:p-9 md:p-16 lg:mt-6 rounded-2xl print:bg-white">
-      <Header />
+    <div className="max-w-3xl p-3 mx-auto my-auto bg-gray-100 border-4 border-gray-700 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 w-letter rounded-2xl print:bg-white">
+      {children}
     </div>
   );
+}
+
+interface PageProps {
+  children?: React.ReactNode
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 export default function Header() {
-  return (
-    <header>
+  const name = "Henry Drummond";
 
+  return (
+    <header className="inline-flex items-baseline justify-between w-full mb-3 align-top border-b-8">
+      <div className="block">
+        <h1 className="mb-0 text-5xl font-bold text-gray-750">{name}</h1>
+        <h2 className="m-0 ml-2 text-2xl font-semibold text-gray-700 leading-snugish">Full Stack Web Development</h2>
+        <h3 className="m-0 mt-2 ml-2 font-semibold text-md text-gray-550 leading-snugish">San Francisco, California</h3>
+      </div>
+      <div className="justify-between px-3 py-3 mt-0 mb-5 text-3xl font-bold leading-none text-gray-200 initials-container bg-gray-750 print:bg-black">
+        <div className="text-center initial">H</div>
+        <div className="text-center initial">D</div>
+      </div>
     </header>
-  )
+  );
 }

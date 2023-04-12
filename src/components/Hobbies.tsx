6ffe62e398eac3eea33aa5108bec78788752a282
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-04-12 17:32:19 -0400

    added hobbies component

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -1,14 +1,14 @@
-export default function Quotes() {
+export default function Hobbies() {
   return (
     <section className="pb-4 mt-4 first:mt-0">
       <div className="break-inside-avoid">
-        <h2 className="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">QUOTES</h2>
+        <h2 className="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">HOBBIES</h2>
         <section className="pb-2 mb-2 border-b-2 break-inside-avoid">
           <ul className="list-inside pr-7">
             <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700 print:">
               <a href="" className="group">
                 <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">•</span>
-                "Think for yourselves and let others enjoy the privilage to do so, too" - voltaire
+                Reading
               </a>
             </li>
             <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700 print:">

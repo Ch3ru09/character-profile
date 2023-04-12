d6b29ba028ee758d386f641c616e4d47576a5ecb
Ch3ru09 <xliang0905@gmail.com>
2023-04-11 20:56:43 -0400

    added page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./Header";
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <></>
-  )
+    <div className="bg-hero-zig-zag">
+      <main className="font-jost hyphens-manual">
+        <Page />
+        <Page />
+      </main>
+    </div>
+  );
+}
+function Page() {
+  return (
+    <div className="max-w-3xl p-3 mx-auto my-auto bg-gray-100 border-4 border-gray-700 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 print:my-o lg:h-letter md:max-w-letter md:h-letter xsm:p-8 sm:p-9 md:p-16 lg:mt-6 rounded-2xl print:bg-white">
+      <Header />
+    </div>
+  );
 }

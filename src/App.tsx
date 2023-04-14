3d7ebbc524d4630f3c77c61846642e8bf914c2ee
Ch3ru09 <xliang0905@gmail.com>
2023-04-13 23:19:30 -0400

    pleaaaase

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
         <Page marginSide={"l"}>
           <Movies />
           <div className="block">
-            <img src="/character-profile/Drummond.jpg" alt="" />
+            <img src="/Drummond.jpg" alt="" />
           </div>
         </Page>
       </main>

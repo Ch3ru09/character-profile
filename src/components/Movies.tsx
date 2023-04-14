36a86916781e37f360b7931a26924a6fecdf39d0
Ch3ru09 <xliang0905@gmail.com>
2023-04-13 23:01:52 -0400

    done

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -8,34 +8,34 @@ export default function Movies() {
             <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md print:">
               <div className="group tooltip hover:text-gray-700">
                 <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">&rsaquo;</span>
-                <a href="https://www.marxists.org/archive/marx/works/download/pdf/Manifesto.pdf" target="_blank">
-                  Manifesto of the Communist Party - Karl Marx
+                <a href="https://www.imdb.com/title/tt0108052/" target="_blank">
+                  Schindler's List - Directed by Steven Spielberg
+                </a>
+                <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">↗</span>
+              </div>
+              <p className="mt-2 ml-4 leading-normal text-gray-700 text-md c-default">Schindler's list is the story of a German saving lives of Jewish people threatened by the Holocaust in Nazi Germany. In a country that despised Jew, Schindler decides to go against that and it resonates well with Drummonds beliefs of fighting what what he thinks is right even while facing opposition.</p>
+            </li>
+            <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md print:">
+              <div className="group tooltip hover:text-gray-700">
+                <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">&rsaquo;</span>
+                <a href="https://www.imdb.com/title/tt0097165/?ref_=nv_sr_srsg_1_tt_7_nm_1_q_the%2520dead%2520poet" target="_blank">
+                  Dead Poets Society - Directed by Peter Weir
+                </a>
+              </div>
+              <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">↗</span>
+              <p className="mt-2 ml-4 leading-normal text-gray-700 text-md c-default">John Keating arrives as a new English teacher at an elite boys' school and inspires his new students to break free from what society want from them and "live more in the moment"; Carpe Diem. Similarly, Henry Drummond is a person that appreciates the encouragement that Keating gave his students to pursue their dreams</p>
+            </li>
+            <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md print:">
+              <div className="group tooltip hover:text-gray-700">
+                <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">&rsaquo;</span>
+                <a href="https://www.imdb.com/title/tt0056592/?ref_=nv_sr_srsg_0_tt_7_nm_1_q_to%2520kill%2520a%2520mo" target="_blank">
+                  To Kill a Mockingbird - Directed by Robert Mulligan
                 </a>
                 <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">↗</span>
               </div>
               <p className="mt-2 ml-4 leading-normal text-gray-700 text-md c-default">
-                Drummond is a person that encourages reform and progress in society and Karl Marx's <em>Manifesto of the Communist Party</em> is one of the most influential political books of all time.
+                Similiarly to his{"[Drummond]"} own trial, Atticus Finch from <em>To Kill a Mockingbird</em> fought against something that he deemed unfair: a black man being falsely accused of raping a white woman. Futhermore, he does this regardless of what the society around him thought of it. Both Atticus and Drummond fought to defend someone that they felt were wronged by their own society. Thus, Drummond like watching this movie for the values that Atticus and himself share.
               </p>
-            </li>
-            <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md print:">
-              <div className="group tooltip hover:text-gray-700">
-                <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">&rsaquo;</span>
-                <a href="https://www.vliz.be/docs/Zeecijfers/Origin_of_Species.pdf" target="_blank">
-                  On the Origin of Species - Charles Darwin
-                </a>
-              </div>
-              <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">↗</span>
-              <p className="mt-2 ml-4 leading-normal text-gray-700 text-md c-default">Drummons is someone that likes to see perspectives of everyone and then makes his mind up after thinking for himself and the origin of humans was a heated topic that he likes thinking about</p>
-            </li>
-            <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md print:">
-              <div className="group tooltip hover:text-gray-700">
-                <span className="mr-2 text-lg font-semibold text-gray-700 leading-snugish">&rsaquo;</span>
-                <a href="https://cdnsm5-ss3.sharpschool.com/UserFiles/Servers/Server_4204286/Image/Grade9GT--TheBookThiefMarkusZusak.pdf" target="_blank">
-                  The Book Thief - Markus Zusak
-                </a>
-                <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">↗</span>
-              </div>
-              <p className="mt-2 ml-4 leading-normal text-gray-700 text-md c-default">The Book Theif is a book about a jewish girl risking her life in nazi Germany to hopefully save a Jew from the german SS and this book shows that no one escapes death so why do we need to kill each other over what we think {"(or whether we are Jew or German)"}. It proves to himself the stupidity of humanity trying to restrict something fundamental like thinking.</p>
             </li>
           </ul>
         </section>
